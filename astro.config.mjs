ac3a1d9dd3cc245bfc181ee206ad04ae0de39f92
therealsilvex <swastickghosh2010@gmail.com>
2026-08-16 18:57:42 +0530

    Remove old tailwind integration to fix v4 build conflict

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,11 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://silvex.pages.dev',
   integrations: [
     react(), 
-    tailwind(), 
     sitemap()
   ],
 });
